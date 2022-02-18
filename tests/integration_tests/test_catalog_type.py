9806bf246f5adb4068ca695ba6018c11bef96956
qtkhajacloud <qtkhajacloud@gmail.com>
2022-02-18 18:13:10 +0530

    Added tests for checking specific items

--- tests/integration_tests/test_catalog_type.py
+++ tests/integration_tests/test_catalog_type.py
@@ -7,6 +7,19 @@ def test_create_catalog_type(client):
     count_before_creation = len(response.json)
     new_catalog_item_json = {"type": "Electronics"}
     response = client.post("/api/v1/catalog/types", json=new_catalog_item_json)
+    item_id = response.json['id']
     assert response.status_code == 201
     response = client.get("/api/v1/catalog/types")
     assert count_before_creation + 1 == len(response.json)
+    # Get the specific item
+    response = client.get(f"/api/v1/catalog/types/{item_id}")
+    assert response.status_code == 200
+    # Delete the specific item
+    response = client.delete(f"/api/v1/catalog/types/{item_id}")
+    response = client.get("/api/v1/catalog/types")
+    assert count_before_creation == len(response.json)
+
+
+
+
+
