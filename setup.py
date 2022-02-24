3e6b16698032ced829a69c8d484bcf8e52c9de8b
qtkhajacloud <qtkhajacloud@gmail.com>
2022-02-24 18:06:42 +0530

    Added tox for test execution in python 3.10

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name="eshop_catalog_service",
+    author="shaik khaja ibrahim",
+    license="Apache License",
+    version="1.0.0",
+    description="This project is a catalog service in an ecommerce application",
+    author_email="qtkhajacloud@gmail.com",
+)
